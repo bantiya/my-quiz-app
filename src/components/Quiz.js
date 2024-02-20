7c9fcf62a86a0aaae342f77fa1b1798913c26e14
Bhavya Antiya <bhavyaantiya@gmail.com>
2024-02-20 12:39:55 -0500

    Finalizing the useState

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,19 +5,21 @@ import { useState } from "react";
 const Quiz = () => {
   // In the useState hook, we pass in an initial value of 1 for the currentQuestionIndex variable.
   // This value will be used to keep track of the current question in the quiz. We can then update this value to move to the next question use the setter function(setQuestionIndex).
-  const [currentQuestionIndex, setQuestionIndex] = useState(1);
-  const testClick = () => {
-    console.log("Next Question button Clicked");
-    setQuestionIndex(currentQuestionIndex + 1);
-  };
+  const [questionNumber, setQuestionNumber] = useState(1);
+
+  const increaseQuestion = () => {
+    setQuestionNumber(questionNumber + 1)
+  }
+
+
 
   return (
     <div className="quiz">
       <div>
-        <div className="score">Question {currentQuestionIndex}/8</div>
+        <div className="score">Question {questionNumber}/8</div>
         <Question />
-        <div className="next-button" onClick={testClick}>
-          Next Question
+        <div className="next-button" onClick={increaseQuestion}>
+          Next Question 
         </div>
       </div>
     </div>
