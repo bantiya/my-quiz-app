6059afc481784eb643cda742899e42f01350f310
Bhavya Antiya <bhavyaantiya@gmail.com>
2024-02-20 12:48:56 -0500

    Object in useState

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,21 +5,27 @@ import { useState } from "react";
 const Quiz = () => {
   // In the useState hook, we pass in an initial value of 1 for the currentQuestionIndex variable.
   // This value will be used to keep track of the current question in the quiz. We can then update this value to move to the next question use the setter function(setQuestionIndex).
-  const [questionNumber, setQuestionNumber] = useState(1);
+  const [state, setState] = useState({
+    currentQuestionIndex: 0,
+    questions: ["Question 1"],
+  });
 
+  // What if you have a lot of different properties in the state it will become very difficult to maintain it so what we use is  : useReducer hook
   const increaseQuestion = () => {
-    setQuestionNumber(questionNumber + 1)
-  }
-
-
+    setState({
+      ...state,
+      currentQuestionIndex: state.currentQuestionIndex + 1,
+    });
+  };
 
   return (
     <div className="quiz">
+        {console.log(state.questions)}
       <div>
-        <div className="score">Question {questionNumber}/8</div>
+        <div className="score">Question {state.currentQuestionIndex}/8</div>
         <Question />
         <div className="next-button" onClick={increaseQuestion}>
-          Next Question 
+          Next Question
         </div>
       </div>
     </div>
